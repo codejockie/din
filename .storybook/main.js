6f13e266a89aaf7d92a3ee39ce0031c8ffe0efcb
John C. Kennedy <devjckennedy@gmail.com>
2023-01-01 20:34:38 -0500

    Add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    // "../stories/**/*.stories.mdx",
+    // "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../packages/date-picker/src/**/*.stories.@(tsx|mdx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react"
+}
